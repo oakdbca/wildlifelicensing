dc9d80e986398ad078aea3632767def34075cc5e
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2021-12-06 16:23:39 +0800

    added new mgt commangd to create returns-report

---
+++ wildlifelicensing/apps/main/management/commands/returns_reports.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from wildlifelicensing.apps.returns.models import Return
+import datetime
+
+
+class Command(BaseCommand):
+    help = 'Run the Returns Report'
+
+    def handle(self, *args, **options):
+        dt = datetime.date(2018,1,1)
+
+        filename = f'returns_report_reg17_{datetime.datetime.now().strftime("%Y%m%d")}.csv'
+        with open(filename, "w") as f:
+            hdr = 'LODGEMENT_NUMBER, LODGEMENT_DATE, STATUS, RETURN_NAME, DATE,FATE,SITE,ZONE,COUNT,DATUM,METHOD,EASTING,MARKING,NAME_ID,SAMPLES,ACCURACY,LATITUDE,LOCATION,NORTHING,CERTAINTY,LONGITUDE,IDENTIFIER,COMMON_NAME,TRANSMITTER,VOUCHER_REF,SPECIES_NAME,SPECIES_GROUP\n'
+            f.write(hdr)
+            for ret in Return.objects.filter(returntable__name__in=['regulation-17'], lodgement_date__gt=dt):
+                for return_table in ret.returntable_set.all():
+                    for return_row in return_table.returnrow_set.all():
+                        data = ",".join(str(val) for val in return_row.data.values())
+                        line = f'{ret.lodgement_number}, {ret.lodgement_date}, {ret.status}, {return_table.name}, {data}\n'
+                        #print(ret.lodgement_number, ret.lodgement_date, ret.status, return_table.name, data)
+
+                        f.write(line)
+
+
+        filename = f'returns_report_reg15_{datetime.datetime.now().strftime("%Y%m%d")}.csv'
+        with open(filename, "w") as f:
+            hdr = 'COMMENTS, NUMBER TAKEN, CONDITION WHEN CAPTURED, DATE COLLECTED/DESTROYED, DATE RELEASED, LOCATION RELEASED, SPECIES, LOCATION COLLECTED\n'
+            f.write(hdr)
+            for ret in Return.objects.filter(returntable__name__in=['regulation-15'], lodgement_date__gt=dt):
+                for return_table in ret.returntable_set.all():
+                    for return_row in return_table.returnrow_set.all():
+                        data = ",".join(str(val) for val in return_row.data.values())
+                        line = f'{ret.lodgement_number}, {ret.lodgement_date}, {ret.status}, {return_table.name}, {data}\n'
+                        #print(ret.lodgement_number, ret.lodgement_date, ret.status, return_table.name, data)
+
+                        f.write(line)
+
