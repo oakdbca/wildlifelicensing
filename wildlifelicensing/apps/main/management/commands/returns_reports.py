0251499f691e7c47b688e73638faa35055e0e04e
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2022-04-04 15:37:52 +0800

    updated delimiter 'returns_reports' csv file

--- wildlifelicensing/apps/main/management/commands/returns_reports.py
+++ wildlifelicensing/apps/main/management/commands/returns_reports.py
@@ -16,13 +16,13 @@ class Command(BaseCommand):
 
         filename1 = f'returns_report_reg17_{datetime.datetime.now().strftime("%Y%m%d")}.csv'
         with open(filename1, "w") as f:
-            hdr = 'LODGEMENT_NUMBER, LICENCE REFERENCE, LODGEMENT_DATE, STATUS, RETURN_NAME, DATE,FATE,SITE,ZONE,COUNT,DATUM,METHOD,EASTING,MARKING,NAME_ID,SAMPLES,ACCURACY,LATITUDE,LOCATION,NORTHING,CERTAINTY,LONGITUDE,IDENTIFIER,COMMON_NAME,TRANSMITTER,VOUCHER_REF,SPECIES_NAME,SPECIES_GROUP\n'
+            hdr = 'LODGEMENT_NUMBER|*|LICENCE REFERENCE|*|LODGEMENT_DATE|*|STATUS|*|RETURN_NAME|*|DATE|*|FATE|*|SITE|*|ZONE|*|COUNT|*|DATUM|*|METHOD|*|EASTING|*|MARKING|*|NAME_ID|*|SAMPLES|*|ACCURACY|*|LATITUDE|*|LOCATION|*|NORTHING|*|CERTAINTY|*|LONGITUDE|*|IDENTIFIER|*|COMMON_NAME|*|TRANSMITTER|*|VOUCHER_REF|*|SPECIES_NAME|*|SPECIES_GROUP\n'
             f.write(hdr)
-            for ret in Return.objects.filter(returntable__name__in=['regulation-17'], lodgement_date__gt=dt)[:1]:
+            for ret in Return.objects.filter(returntable__name__in=['regulation-17'], lodgement_date__gt=dt):
                 for return_table in ret.returntable_set.all():
                     for return_row in return_table.returnrow_set.all():
-                        data = ",".join(str(val) for val in return_row.data.values())
-                        line = f'{ret.lodgement_number}, {ret.licence.reference}, {ret.lodgement_date}, {ret.status}, {return_table.name}, {data}\n'
+                        data = "|*|".join(str(val) for val in return_row.data.values())
+                        line = f'{ret.lodgement_number}|*|{ret.licence.reference}|*|{ret.lodgement_date}|*|{ret.status}|*|{return_table.name}|*|{data}\n'
                         #print(ret.lodgement_number, ret.lodgement_date, ret.status, return_table.name, data)
 
                         f.write(line)
@@ -30,36 +30,36 @@ class Command(BaseCommand):
 
         filename2 = f'returns_report_reg15_{datetime.datetime.now().strftime("%Y%m%d")}.csv'
         with open(filename2, "w") as f:
-            hdr = 'LODGEMENT_NUMBER, LICENCE REFERENCE, LODGEMENT_DATE, STATUS, RETURN_NAME, COMMENTS, NUMBER TAKEN, CONDITION WHEN CAPTURED, DATE COLLECTED/DESTROYED, DATE RELEASED, LOCATION RELEASED, SPECIES, LOCATION COLLECTED\n'
+            hdr = 'LODGEMENT_NUMBER|*|LICENCE REFERENCE|*|LODGEMENT_DATE|*|STATUS|*|RETURN_NAME|*|COMMENTS|*|NUMBER TAKEN|*|CONDITION WHEN CAPTURED|*|DATE COLLECTED/DESTROYED|*|DATE RELEASED|*|LOCATION RELEASED|*|SPECIES|*|LOCATION COLLECTED\n'
             f.write(hdr)
-            for ret in Return.objects.filter(returntable__name__in=['regulation-15'], lodgement_date__gt=dt)[:1]:
+            for ret in Return.objects.filter(returntable__name__in=['regulation-15'], lodgement_date__gt=dt):
                 for return_table in ret.returntable_set.all():
                     for return_row in return_table.returnrow_set.all():
-                        data = ",".join(str(val) for val in return_row.data.values())
-                        line = f'{ret.lodgement_number}, {ret.licence.reference}, {ret.lodgement_date}, {ret.status}, {return_table.name}, {data}\n'
+                        data = "|*|".join(str(val) for val in return_row.data.values())
+                        line = f'{ret.lodgement_number}|*|{ret.licence.reference}|*|{ret.lodgement_date}|*|{ret.status}|*|{return_table.name}|*|{data}\n'
                         #print(ret.lodgement_number, ret.lodgement_date, ret.status, return_table.name, data)
 
                         f.write(line)
 
 
-        with ZipFile('returns_reports.zip', 'w') as zipObj:
+        with ZipFile(f'returns_reports_{datetime.datetime.now().strftime("%Y%m%d")}.zip', 'w') as zipObj:
             # Add multiple files to the zip
             zipObj.write(filename1)
             zipObj.write(filename2)
 
-        email = EmailMessage()
-        email.subject = 'Wildlife Licensing Returns Report'
-        email.body = 'Wildlife Licensing Returns Report'
-        email.from_email = settings.EMAIL_FROM 
-        email.to = settings.REPORTS_EMAIL if isinstance(settings.REPORTS_EMAIL, list) else [settings.REPORTS_EMAIL]
-        email.attach_file('returns_reports.zip')
-
-        res = email.send()
-
-        # cleanup
-        os.remove(filename1)
-        os.remove(filename2)
-        os.remove('returns_reports.zip')
+#        email = EmailMessage()
+#        email.subject = 'Wildlife Licensing Returns Report'
+#        email.body = 'Wildlife Licensing Returns Report'
+#        email.from_email = settings.EMAIL_FROM 
+#        email.to = settings.REPORTS_EMAIL if isinstance(settings.REPORTS_EMAIL, list) else [settings.REPORTS_EMAIL]
+#        email.attach_file('returns_reports.zip')
+#
+#        res = email.send()
+#
+#        # cleanup
+#        os.remove(filename1)
+#        os.remove(filename2)
+#        os.remove('returns_reports.zip')
 
 
 
