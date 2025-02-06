2fed01f23938b10fac3ee08b43f4360c9547861d
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-02-06 14:31:39 +0800

    add ledger api client to install apps.

--- wildlifelicensing/settings.py
+++ wildlifelicensing/settings.py
@@ -20,6 +20,7 @@ ROOT_URLCONF = "wildlifelicensing.urls"
 SITE_ID = 1
 INSTALLED_APPS += [
     "bootstrap3",
+    "ledger_api_client",
     "wildlifelicensing.apps.dashboard",
     "wildlifelicensing.apps.main",
     "wildlifelicensing.apps.applications",
