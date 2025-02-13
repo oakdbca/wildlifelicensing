a662ba4cab9fe8ee128e669ad07ae2941a3412cf
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-02-13 10:23:12 +0800

    Update url params.

--- wildlifelicensing/urls.py
+++ wildlifelicensing/urls.py
@@ -7,34 +7,54 @@ from wildlifelicensing.apps.dashboard.views.base import DashBoardRoutingView
 urlpatterns = [
     url(r"^ledger/admin/", admin.site.urls, name="ledger_admin"),
     url(r"^$", DashBoardRoutingView.as_view(), name="wl_home"),
-    url(r"", include("wildlifelicensing.apps.main.urls", namespace="wl_main")),
     url(
-        r"", include("wildlifelicensing.apps.dashboard.urls", namespace="wl_dashboard")
+        r"",
+        include(("wildlifelicensing.apps.main.urls", "wl_main"), namespace="wl_main"),
+    ),
+    url(
+        r"",
+        include(
+            ("wildlifelicensing.apps.dashboard.urls", "wl_dashboard"),
+            namespace="wl_dashboard",
+        ),
     ),
     url(
         r"^applications/",
         include(
-            "wildlifelicensing.apps.applications.urls", namespace="wl_applications"
+            ("wildlifelicensing.apps.applications.urls", "wl_applications"),
+            namespace="wl_applications",
         ),
     ),
     url(
         r"^customer_management/",
         include(
-            "wildlifelicensing.apps.customer_management.urls",
+            (
+                "wildlifelicensing.apps.customer_management.urls",
+                "wl_customer_management",
+            ),
             namespace="wl_customer_management",
         ),
     ),
     url(
         r"^reports/",
-        include("wildlifelicensing.apps.reports.urls", namespace="wl_reports"),
+        include(
+            ("wildlifelicensing.apps.reports.urls", "wl_reports"),
+            namespace="wl_reports",
+        ),
     ),
     url(
         r"^returns/",
-        include("wildlifelicensing.apps.returns.urls", namespace="wl_returns"),
+        include(
+            ("wildlifelicensing.apps.returns.urls", "wl_returns"),
+            namespace="wl_returns",
+        ),
     ),
     url(
         r"^payments/",
-        include("wildlifelicensing.apps.payments.urls", namespace="wl_payments"),
+        include(
+            ("wildlifelicensing.apps.payments.urls", "wl_payments"),
+            namespace="wl_payments",
+        ),
     ),
 ]
 
