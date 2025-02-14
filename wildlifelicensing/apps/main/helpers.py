bcd5dfb7fb48eaeed11112b3118315ef3269a959
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-02-14 15:28:54 +0800

    Update helpers to use system groups instead of auth groups.

--- wildlifelicensing/apps/main/helpers.py
+++ wildlifelicensing/apps/main/helpers.py
@@ -1,16 +1,45 @@
 from django.conf import settings
+from django.core.cache import cache
 from ledger_api_client.ledger_models import EmailUserRO as EmailUser
+from ledger_api_client.managed_models import SystemGroup
+
+
+def superuser_ids_list() -> list:
+    cache_key = settings.CACHE_KEY_SUPERUSER_IDS
+    superuser_ids = cache.get(cache_key)
+    if superuser_ids is None:
+        superuser_ids = list(
+            EmailUser.objects.filter(is_superuser=True).values_list("id", flat=True)
+        )
+        cache.set(cache_key, superuser_ids, settings.CACHE_TIMEOUT_5_SECONDS)
+    return superuser_ids
+
+
+def belongs_to_by_user_id(user_id: int, group_name: str) -> bool:
+    superuser_ids = superuser_ids_list()
+    if superuser_ids and user_id in superuser_ids:
+        return True
+    cache_key = settings.CACHE_KEY_USER_BELONGS_TO_GROUP.format(
+        **{"user_id": user_id, "group_name": group_name}
+    )
+    belongs_to = cache.get(cache_key)
+    if belongs_to is None:
+        system_group = SystemGroup.objects.filter(name=group_name).first()
+        belongs_to = (
+            system_group and user_id in system_group.get_system_group_member_ids()
+        )
+        cache.set(cache_key, belongs_to, settings.CACHE_TIMEOUT_5_SECONDS)
+    return belongs_to
 
 
 # TODO: Change to Ledger api client System Groups
-def belongs_to(user, group_name):
-    """
-    Check if the user belongs to the given group.
-    :param user:
-    :param group_name:
-    :return:
-    """
-    return user.groups.filter(name=group_name).exists()
+def belongs_to(user: EmailUser, group_name: str) -> bool:
+    if not user.is_authenticated:
+        return False
+    if user.is_superuser:
+        return True
+
+    return belongs_to_by_user_id(user.id, group_name)
 
 
 def is_customer(user):
