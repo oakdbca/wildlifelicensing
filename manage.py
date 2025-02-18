182423eb8a1e40e4bc68e4b9dcac6b4f4ec45546
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-02-18 14:12:25 +0800

    decouple not needed here as django conf settings are imported when importing execute_from_command_line.

--- manage.py
+++ manage.py
@@ -2,9 +2,6 @@
 import os
 import sys
 
-# This import will automatically find the .env file and load the environment variables
-from decouple import config  # noqa
-
 if __name__ == "__main__":
     path = os.path.dirname(os.path.abspath(__file__))
     # Remove trailing slash
@@ -12,6 +9,7 @@ if __name__ == "__main__":
         path = path[:-1]
     project_folder_name = os.path.basename(path)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"{project_folder_name}.settings")
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
