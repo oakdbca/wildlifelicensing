fce4daa47c3449c16a16c13130e846cf0a5be58f
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-02-19 16:46:42 +0800

    Add correct file extenstion.

--- gunicorn.ini.py
+++ gunicorn.ini.py
@@ -2,11 +2,13 @@
 import multiprocessing
 
 # Don't start too many workers:
-workers = min(multiprocessing.cpu_count() * 2 + 1, 16)
+workers = min(multiprocessing.cpu_count() * 2 + 1, 4)
+
 # Give workers an expiry:
 max_requests = 2048
 max_requests_jitter = 256
 preload_app = True
 timeout = 600
+
 # Disable access logging.
 accesslog = None
