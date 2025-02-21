ad32884a4903e1e1448ce341a0dae1da9a523686
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-02-21 14:03:02 +0800

    Update serializers to match the formatting done by the preserialize post hook.

--- wildlifelicensing/apps/applications/serializers.py
+++ wildlifelicensing/apps/applications/serializers.py
@@ -14,6 +14,9 @@ logger = logging.getLogger(__name__)
 
 
 class ApplicationApplicantSerializer(serializers.ModelSerializer):
+    identification2 = serializers.SerializerMethodField()
+    senior_card2 = serializers.SerializerMethodField()
+
     class Meta:
         model = EmailUser
         exclude = (
@@ -21,6 +24,12 @@ class ApplicationApplicantSerializer(serializers.ModelSerializer):
             "postal_address",
             "billing_address",
         )
+
+    def get_identification2(self, obj):
+        return obj.identification2.upload.url if obj.identification2 else None
+
+    def get_senior_card2(self, obj):
+        return obj.senior_card2.upload.url if obj.senior_card2 else None
 
 
 class ApplicationProfileSerializer(serializers.ModelSerializer):
@@ -68,9 +77,17 @@ class ApplicationLicenceSerializer(serializers.ModelSerializer):
 
 
 class ApplicationWildlifeLicenceTypeSerializer(serializers.ModelSerializer):
+    default_conditions = serializers.SerializerMethodField()
+
     class Meta:
         model = WildlifeLicenceType
         fields = "__all__"
+
+    def get_default_conditions(self, obj):
+        return [
+            ap.condition
+            for ap in obj.licence_type.defaultcondition_set.order_by("order")
+        ]
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
@@ -79,7 +96,20 @@ class ApplicationSerializer(serializers.ModelSerializer):
     assigned_officer = ApplicationApplicantSerializer()
     applicant_profile = ApplicationProfileSerializer()
     licence_type = ApplicationWildlifeLicenceTypeSerializer()
+    processing_status = serializers.CharField(source="get_processing_status_display")
+    id_check_status = serializers.CharField(source="get_id_check_status_display")
+    returns_check_status = serializers.CharField(
+        source="get_returns_check_status_display"
+    )
+    character_check_status = serializers.CharField(
+        source="get_character_check_status_display"
+    )
+    review_status = serializers.CharField(source="get_review_status_display")
+    conditions = serializers.SerializerMethodField()
 
     class Meta:
         model = Application
         fields = "__all__"
+
+    def get_conditions(self, obj):
+        return [ap.condition for ap in obj.applicationcondition_set.order_by("order")]
