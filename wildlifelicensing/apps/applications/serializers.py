05e681794291beb69724ce8a41b415a602df9929
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-02-26 13:47:50 +0800

    Required mods to serializers.

--- wildlifelicensing/apps/applications/serializers.py
+++ wildlifelicensing/apps/applications/serializers.py
@@ -4,9 +4,11 @@ from ledger_api_client.ledger_models import EmailUserRO as EmailUser
 from rest_framework import serializers
 
 from wildlifelicensing.apps.applications.models import (
+    AmendmentRequest,
     Application,
     ApplicationLogEntry,
     ApplicationRequest,
+    ApplicationUserAction,
     Assessment,
 )
 from wildlifelicensing.apps.main.helpers import render_user_name
@@ -17,7 +19,11 @@ from wildlifelicensing.apps.main.models import (
     WildlifeLicence,
     WildlifeLicenceType,
 )
-from wildlifelicensing.apps.main.serializers import EmailUserWithoutAddressesSerializer
+from wildlifelicensing.apps.main.serializers import (
+    AddressSerializer,
+    CommunicationsLogEntrySerializer,
+    EmailUserWithoutAddressesSerializer,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -46,9 +52,12 @@ class ApplicationApplicantSerializer(serializers.ModelSerializer):
 
 
 class ApplicationProfileSerializer(serializers.ModelSerializer):
+    user = ApplicationApplicantSerializer()
+    postal_address = AddressSerializer()
+
     class Meta:
         model = Profile
-        fields = ("email", "id", "institution", "name")
+        fields = ("user", "email", "id", "institution", "name", "postal_address")
 
 
 class PreviousApplicationSerializer(serializers.ModelSerializer):
@@ -97,7 +106,10 @@ class ApplicationWildlifeLicenceTypeSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def get_default_conditions(self, obj):
-        return [dc for dc in obj.default_conditions.order_by("defaultcondition__order")]
+        return [
+            ConditionSerializer(dc).data
+            for dc in obj.default_conditions.order_by("defaultcondition__order")
+        ]
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
@@ -122,7 +134,10 @@ class ApplicationSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def get_conditions(self, obj):
-        return [ap.condition for ap in obj.applicationcondition_set.order_by("order")]
+        return [
+            ConditionSerializer(ap.condition).data
+            for ap in obj.applicationcondition_set.order_by("order")
+        ]
 
 
 class AssessmentOfficerSerializer(serializers.ModelSerializer):
@@ -157,42 +172,53 @@ class AssessmentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Assessment
-        exclude = ("application", "applicationrequest_ptr")
+        exclude = ("application",)
 
     def get_assessor_group(self, obj):
-        return obj.assessor
+        return obj.assessor_group.name if obj.assessor_group else None
+
+    def get_conditions(self, obj):
+        return [
+            {
+                "acceptance_status": ac.get_acceptance_status_display(),
+                "id": ac.condition.id,
+                "condition": ConditionSerializer(ac.condition.condition).data,
+            }
+            for ac in obj.assessmentcondition_set.all()
+        ]
 
 
-class ConditionSerializer(serializers.Serializer):
+class ConditionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Condition
         fields = "__all__"
 
 
-class ApplicationLogEntrySerializer(serializers.Serializer):
-    type = serializers.CharField(source="get_type_display")
-    documents = serializers.SerializerMethodField()
+class ApplicationLogEntrySerializer(serializers.ModelSerializer):
+    communicationslogentry_ptr = serializers.SerializerMethodField()
 
     class Meta:
         model = ApplicationLogEntry
         exclude = ("application", "customer", "staff")
 
-    def get_documents(self, obj):
-        return [(str(document), document.file.url) for document in obj.documents.all()]
+    def get_communicationslogentry_ptr(self, obj):
+        if not obj or not hasattr(obj, "communicationslogentry_ptr"):
+            return None
+        return CommunicationsLogEntrySerializer(obj.communicationslogentry_ptr).data
 
 
-class ApplicationUserActionSerializer(serializers.Serializer):
+class ApplicationUserActionSerializer(serializers.ModelSerializer):
     who = serializers.SerializerMethodField()
 
     class Meta:
-        model = Application
+        model = ApplicationUserAction
         fields = ("who", "when", "what")
 
     def get_who(self, obj):
         return render_user_name(obj.who)
 
 
-class ApplicationRequestSerializer(serializers.Serializer):
+class ApplicationRequestSerializer(serializers.ModelSerializer):
     application = ApplicationSerializer()
     officer = EmailUserWithoutAddressesSerializer()
 
@@ -201,12 +227,14 @@ class ApplicationRequestSerializer(serializers.Serializer):
         fields = "__all__"
 
 
-class AmendmentRequestSerializer(serializers.Serializer):
-    reason = serializers.CharField(source="get_reason_display")
-    application = ApplicationSerializer()
-    officer = EmailUserWithoutAddressesSerializer()
-    applicationrequest_ptr = ApplicationRequestSerializer()
+class AmendmentRequestSerializer(serializers.ModelSerializer):
+    applicationrequest_ptr = serializers.SerializerMethodField()
 
     class Meta:
-        model = Application
+        model = AmendmentRequest
         fields = "__all__"
+
+    def get_applicationrequest_ptr(self, obj):
+        if not obj:
+            return None
+        return ApplicationRequestSerializer(obj.applicationrequest_ptr).data
