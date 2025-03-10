f81e7e24baeca0341342f27be474ff8d8c310bfd
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-03-10 13:46:48 +0800

    Add LEDGER_UI_URL and PAYMENT_SYSTEM_PREFIX.

--- wildlifelicensing/context_processors.py
+++ wildlifelicensing/context_processors.py
@@ -5,4 +5,6 @@ def config(request):
     return {
         "template_group": settings.TEMPLATE_GROUP,
         "template_title": settings.TEMPLATE_TITLE,
+        "LEDGER_UI_URL": settings.LEDGER_UI_URL,
+        "PAYMENT_SYSTEM_PREFIX": settings.PAYMENT_SYSTEM_PREFIX,
     }
