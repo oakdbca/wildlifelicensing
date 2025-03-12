671112a38fe4ee753d08300081a84aa764e6c307
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-03-12 15:54:12 +0800

    Replace two migrations with just one that doesn't have the default uuid value.

--- wildlifelicensing/apps/applications/migrations/0026_application_payment_uuid.py
+++ wildlifelicensing/apps/applications/migrations/0026_application_payment_uuid.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="application",
             name="payment_uuid",
-            field=models.UUIDField(default=uuid.uuid4, editable=False, null=True),
+            field=models.UUIDField(editable=False, null=True),
         ),
     ]
