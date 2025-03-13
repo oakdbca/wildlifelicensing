a9c977094686af02127781cc3262df58af96a51e
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-03-13 15:06:09 +0800

    Set input formats and widget for from_date and to_date in ReportForm so values don't disappear after the page is loaded.

--- wildlifelicensing/apps/reports/forms.py
+++ wildlifelicensing/apps/reports/forms.py
@@ -1,27 +1,37 @@
-from django import forms
-from datetime import datetime, date
+from datetime import date, datetime
 
+from django import forms
+from django.conf import settings
 from django.forms.widgets import SelectMultiple
+
 from wildlifelicensing.apps.main.models import Region
 
 
 class ReportForm(forms.Form):
-    from_date = forms.DateField(required=True)
-    to_date = forms.DateField(required=True)
-    regions = forms.ModelMultipleChoiceField(queryset=Region.objects.all(),
-                                             required=False,
-                                             widget=SelectMultiple(
-                                                 attrs={"class": "hidden"}
-                                             ))
+    from_date = forms.DateField(
+        input_formats=[settings.DEFAULT_FORM_DATE_FORMAT],
+        required=True,
+        widget=forms.DateTimeInput(format=settings.DEFAULT_FORM_DATE_FORMAT),
+    )
+    to_date = forms.DateField(
+        input_formats=[settings.DEFAULT_FORM_DATE_FORMAT],
+        required=True,
+        widget=forms.DateTimeInput(format=settings.DEFAULT_FORM_DATE_FORMAT),
+    )
+    regions = forms.ModelMultipleChoiceField(
+        queryset=Region.objects.all(),
+        required=False,
+        widget=SelectMultiple(attrs={"class": "hidden"}),
+    )
 
     def __init__(self, *args, **kwargs):
-        super(ReportForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
         today = datetime.today()
 
         if today.month < 7:
-            self.fields['from_date'].initial = date(today.year - 1, 7, 1)
-            self.fields['to_date'].initial = date(today.year, 6, 30)
+            self.fields["from_date"].initial = date(today.year - 1, 7, 1)
+            self.fields["to_date"].initial = date(today.year, 6, 30)
         else:
-            self.fields['from_date'].initial = date(today.year, 7, 1)
-            self.fields['to_date'].initial = date(today.year + 1, 6, 30)
+            self.fields["from_date"].initial = date(today.year, 7, 1)
+            self.fields["to_date"].initial = date(today.year + 1, 6, 30)
