2315b5290177e79affbf54fba14e9ba19ec2224d
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-03-13 09:33:40 +0800

    Update View Payment link to use new invoice-pdf url that fetches the appropriate invoice pdf via ledger api client.

--- wildlifelicensing/apps/dashboard/views/officer.py
+++ wildlifelicensing/apps/dashboard/views/officer.py
@@ -361,13 +361,13 @@ class DataTableApplicationsOfficerView(
             )
 
         if obj.invoice_reference:
-            # url = "{}?invoice={}".format(
-            #     reverse("payments:invoice-payment"), obj.invoice_reference
-            # )
-            # action += r'<br \><a target="_blank" href="{}"> View Payment</a>'.format(
-            #     url
-            # )
-            action += r'<br \><a href="#">TODO: Ledger API View Payment</a>'
+            url = reverse(
+                "wl_payments:invoice-pdf",
+                kwargs={"invoice_reference": obj.invoice_reference},
+            )
+            action += r'<br \><a target="_blank" href="{}"> View Payment</a>'.format(
+                url
+            )
 
         return action
 
