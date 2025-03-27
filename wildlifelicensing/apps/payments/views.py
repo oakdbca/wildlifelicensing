1094a9ca33fd1ee125db7e6809c67fe9664aac6f
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-03-27 14:24:10 +0800

    Resolve TODO comment by changing to note as this section of code is no longer as important as I thought.

--- wildlifelicensing/apps/payments/views.py
+++ wildlifelicensing/apps/payments/views.py
@@ -78,7 +78,9 @@ class CheckoutApplicationView(LoginRequiredMixin, RedirectView):
         ):
             discount = Decimal(product.price * Decimal("0.1")).quantize(
                 Decimal("0.00")
-            )  # TODO: What type of discount do seniors get?
+            )  # Note: Currently I have just hardcoded the discount to 10% of the product price
+            # However, I have found that none of the licences currently being offered allow for a senior discount
+            # so this shouldn't actually happen in practice
             products.append(
                 {
                     "ledger_description": "Senior Discount",
