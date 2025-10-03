2ad8dd2e778c0da065a7021fd68d50ba166ee3c8
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-10-03 16:08:12 +0800

    Fix conditional syntax for ENABLE_CRON and ENABLE_WEB checks in startup script

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 
-if [ $ENABLE_CRON == "True" ];
+if [ "$ENABLE_CRON" = "True" ];
     then
     echo "Starting Python Cron"
     python /bin/scheduler.py /app/python-cron /app/logs/python-cron.log &
@@ -15,7 +15,7 @@ else
    echo "ENABLE_CRON environment variable not set to True, python cron is not starting."
 fi
 
-if [ $ENABLE_WEB == "True" ];
+if [ "$ENABLE_WEB" = "True" ];
     then
     echo "Starting Gunicorn"
     /app/venv/bin/gunicorn  wildlifelicensing.wsgi --bind :8080 --config /app/gunicorn.ini.py
