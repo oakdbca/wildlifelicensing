99e81af986ec27fe75113b8bf510cce60f559ad2
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-10-15 13:35:08 +0800

    Remove bootstrap 3 typeahead.

--- wildlifelicensing/static/wl/js/config.js
+++ wildlifelicensing/static/wl/js/config.js
@@ -7,8 +7,9 @@ require.config({
       "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min",
     "bootstrap-datetimepicker":
       "//static.dpaw.wa.gov.au/static/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min",
-    // Use Select2 v4 to match the v4 CSS/theme loaded from templates
-    select2: "https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min",
+    // Use Select2 v4 full bundle (includes compat modules) to work with RequireJS
+    select2:
+      "https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.full.min",
     handlebars:
       "//static.dpaw.wa.gov.au/static/libs/handlebars.js/4.0.5/handlebars.amd.min",
     "handlebars.runtime":
@@ -23,8 +24,6 @@ require.config({
     lodash: "//static.dpaw.wa.gov.au/static/libs/lodash.js/4.5.1/lodash.min",
     "bootstrap.treeView":
       "//static.dpaw.wa.gov.au/static/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min",
-    "bootstrap-3-typeahead":
-      "//static.dpaw.wa.gov.au/static/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min",
     "datatables.datetime":
       "//cdn.datatables.net/plug-ins/1.10.11/sorting/datetime-moment",
   },
@@ -61,9 +60,6 @@ require.config({
       deps: ["jQuery"],
     },
     "bootstrap.treeView": {
-      deps: ["bootstrap"],
-    },
-    "bootstrap-3-typeahead": {
       deps: ["bootstrap"],
     },
   },
