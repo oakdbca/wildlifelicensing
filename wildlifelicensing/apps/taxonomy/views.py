9066138c093b20ef2367b5d478109bf31c507e41
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-11-04 12:55:20 +0800

    Refactor SpeciesNamesJSON view to use JsonResponse and streamline search results retrieval

--- wildlifelicensing/apps/taxonomy/views.py
+++ wildlifelicensing/apps/taxonomy/views.py
@@ -1,10 +1,10 @@
 import logging
 
-from django.http import HttpResponse
+from django.conf import settings
+from django.http import JsonResponse
 from django.views.generic import View
 
 from wildlifelicensing.apps.main.models import NomosTaxonomy
-from wildlifelicensing.apps.main.serializers import NomosTaxonomySerializer
 
 logger = logging.getLogger(__name__)
 
@@ -13,11 +13,14 @@ class SpeciesNamesJSON(View):
     def get(self, request, *args, **kwargs):
         search = request.GET.get("search", None)
         if not search:
-            return HttpResponse(
-                [],
-                content_type="application/json",
-            )
-        serializer = NomosTaxonomySerializer(
-            NomosTaxonomy.objects.filter(name__icontains=search), many=True
+            return JsonResponse([], safe=False)
+
+        names_qs = (
+            NomosTaxonomy.objects
+            .filter(name__icontains=search)
+            .values_list("name", flat=True)
+            .distinct()[:settings.NOMOS_MAXIMUM_SEARCH_RESULTS]
         )
-        return HttpResponse(serializer.data, content_type="application/json")
+
+        names = list(names_qs)
+        return JsonResponse(names, safe=False)
